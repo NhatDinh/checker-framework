2fad0f1b8574e1430dd2fc903aee704d86035c05
nd <nhat.ldinh@gmail.com>
2018-07-24 13:22:28 -0700

    assigned component of Char type to NN recursively

--- checker/src/main/java/org/checkerframework/checker/index/lowerbound/LowerBoundAnnotatedTypeFactory.java
+++ checker/src/main/java/org/checkerframework/checker/index/lowerbound/LowerBoundAnnotatedTypeFactory.java
@@ -14,7 +14,6 @@ import java.util.LinkedHashSet;
 import java.util.Set;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.Element;
-import javax.lang.model.type.DeclaredType;
 import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import org.checkerframework.checker.index.IndexMethodIdentifier;
@@ -51,7 +50,6 @@ import org.checkerframework.framework.type.treeannotator.TreeAnnotator;
 import org.checkerframework.javacutil.AnnotationBuilder;
 import org.checkerframework.javacutil.AnnotationUtils;
 import org.checkerframework.javacutil.TreeUtils;
-import org.checkerframework.javacutil.TypesUtils;
 
 /**
  * Implements the introduction rules for the Lower Bound Checker.
@@ -175,24 +173,16 @@ public class LowerBoundAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
                 if (qualHierarchy.isSubtype(NN, type.getAnnotationInHierarchy(UNKNOWN)))
                     type.replaceAnnotation(NN);
                 break;
-            case DECLARED:
-                String qualifiedName =
-                        TypesUtils.getQualifiedName((DeclaredType) type.getUnderlyingType())
-                                .toString();
-                if (qualifiedName.equals("java.lang.Character")) {
-                    if (qualHierarchy.isSubtype(NN, type.getAnnotationInHierarchy(UNKNOWN))) {
-                        type.replaceAnnotation(NN);
-                    }
-                }
             case ARRAY:
+                AnnotatedTypeMirror componentType;
                 AnnotatedTypeMirror.AnnotatedArrayType annotatedArrayType =
                         ((AnnotatedTypeMirror.AnnotatedArrayType) type);
-                AnnotatedTypeMirror componentType = annotatedArrayType.getComponentType();
+                componentType = annotatedArrayType.getComponentType();
                 if (componentType != null) {
-                    ensureCharNonNegative((AnnotatedTypeMirror) componentType);
+                    ensureCharNonNegative(componentType);
                     componentType = annotatedArrayType.getComponentType();
-                    break;
                 }
+                break;
         }
     }
 
